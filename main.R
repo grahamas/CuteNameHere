cba1d85ee1358e7be9e77f92cf6e83229c9f4ef0
Graham Smith <grahamas@gmail.com>
2019-07-23 18:25:19 -0500

    my guesstimate wasn't that bad!

--- main.R
+++ main.R
@@ -1,46 +1,94 @@
 
 t0 = 0
-a = 35000 * 0.95
+a = 35000
 r = 0.05
 acc = 0
 for (i in 1:2) {
   acc = acc * (1 + r)
-  acc = acc + (a - 30000)
+  inc = a - max(0, (a - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, a - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, a - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, a - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, a - 9525) * 0.12)
+  inc = inc - max(0, min(9525, a) * 0.1)
+  acc = acc + inc - 30000
 }
-b = 70000 * 0.88
+b = 70000
 for (i in 1:5) {
   acc = acc * (1 + r)
-  acc = acc + (b - 30000)
+  inc = b - max(0, (b - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, b - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, b - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, b - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, b - 9525) * 0.12)
+  inc = inc - max(0, min(9525, b) * 0.1)
+  acc = acc + inc - 30000
 }
-c = 100000 * 0.83
+c = 100000
 for (i in 1:1) {
   acc = acc * (1 + r)
-  acc = acc + (c - 50000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 50000
 }
-c = 180000 * 0.75
+c = 180000
 for (i in 1:2) {
   acc = acc * (1 + r)
-  acc = acc + (c - 100000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 100000
 }
-c = 180000 * 0.75
+c = 180000
 for (i in 1:9) {
   acc = acc * (1 + r)
-  acc = acc + (c - 150000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 150000
 }
-c = 250000 * 0.7
+c = 250000
 for (i in 1:9) {
   acc = acc * (1 + r)
-  acc = acc + (c - 150000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 150000
 }
-c = 250000 * 0.7
+c = 250000
 for (i in 1:6) {
   acc = acc * (1 + r)
-  acc = acc + (c - 210000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 210000
 }
-c = 250000 * 0.7
+c = 250000
 for (i in 1:11) {
   acc = acc * (1 + r)
-  acc = acc + (c - 100000)
+  inc = c - max(0, (c - 200000) * 0.35)
+  inc = inc - max(0, min(200000 - 157500, c - 157500) * 0.32)
+  inc = inc - max(0, min(157500 - 82500, c - 82500) * 0.24)
+  inc = inc - max(0, min(82500 - 38700, c - 38700) * 0.22)
+  inc = inc - max(0, min(38700 - 9525, c - 9525) * 0.12)
+  inc = inc - max(0, min(9525, c) * 0.1)
+  acc = acc + inc - 100000
 }
 print(acc)
 
